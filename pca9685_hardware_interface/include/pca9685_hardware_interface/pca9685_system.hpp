686e33c3742e6fede5216b3139263ff0d9af2c52
Christian Rauch <Rauch.Christian@gmx.de>
2026-02-08 12:25:47 +0100

    move class 'PCA9685'

--- pca9685_hardware_interface/include/pca9685_hardware_interface/pca9685_system.hpp
+++ pca9685_hardware_interface/include/pca9685_hardware_interface/pca9685_system.hpp
@@ -17,7 +17,88 @@
 #include "rclcpp_lifecycle/state.hpp"
 
 #include "pca9685_hardware_interface/visibility_control.h"
-#include <pca9685_hardware_interface/pca9685_comm.h>
+
+#include <sys/ioctl.h>
+#include <fcntl.h>
+extern "C" {
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
+}
+
+
+class PCA9685 {
+public:
+  explicit PCA9685(const std::string &device = "/dev/i2c-1", int address = 0x40) {
+    bus_fd = open(device.c_str(), O_RDWR);
+    if(bus_fd < 0) {
+      throw std::system_error(errno, std::system_category(), "Could not open i2c bus.");
+    }
+
+    if(ioctl(bus_fd, I2C_SLAVE, address) < 0) {
+      throw std::system_error(errno, std::system_category(), "Could not set peripheral address.");
+    }
+
+    // OUTDRV: totem pole structure
+    i2c_smbus_write_byte_data(bus_fd, R_MODE2, 0b0000'0100);
+    usleep(5'000);
+  }
+
+  ~PCA9685() {
+    close(bus_fd);
+  }
+
+  void set_pwm_freq(const double freq_hz) {
+    frequency = freq_hz;
+
+    auto prescaleval = 2.5e7; //    # 25MHz
+    prescaleval /= 4096.0; //       # 12-bit
+    prescaleval /= freq_hz;
+    prescaleval -= 1.0;
+
+    auto prescale = static_cast<int>(std::round(prescaleval));
+
+    const auto oldmode = i2c_smbus_read_byte_data(bus_fd, R_MODE1);
+
+    auto newmode = (oldmode & 0x7F) | 0b0001'0000;
+
+    i2c_smbus_write_byte_data(bus_fd, R_MODE1, newmode);
+    i2c_smbus_write_byte_data(bus_fd, R_PRESCALE, prescale);
+    i2c_smbus_write_byte_data(bus_fd, R_MODE1, oldmode);
+    usleep(5'000);
+    i2c_smbus_write_byte_data(bus_fd, R_MODE1, oldmode | 0b1000'0000);
+  }
+
+  void set_pwm(const int channel, const uint16_t on, const uint16_t off) {
+    const auto channel_offset = 4 * channel;
+    i2c_smbus_write_byte_data(bus_fd, R_LED0_ON_L+channel_offset, on & 0xFF);
+    i2c_smbus_write_byte_data(bus_fd, R_LED0_ON_H+channel_offset, on >> 8);
+    i2c_smbus_write_byte_data(bus_fd, R_LED0_OFF_L+channel_offset, off & 0xFF);
+    i2c_smbus_write_byte_data(bus_fd, R_LED0_OFF_H+channel_offset, off >> 8);
+  }
+
+  void set_pwm_ms(const int channel, const double ms) {
+    auto period_ms = 1000.0 / frequency;
+    auto bits_per_ms = 4096 / period_ms;
+    auto bits = ms * bits_per_ms;
+    set_pwm(channel, 0, bits);
+  }
+
+private:
+  int bus_fd;
+
+  // Default frequency pulled from PCA9685 datasheet.
+  double frequency = 200.0;
+
+  static constexpr uint8_t R_MODE1            = 0x00;
+  static constexpr uint8_t R_MODE2            = 0x01;
+  static constexpr uint8_t R_PRESCALE         = 0xFE;
+  static constexpr uint8_t R_LED0_ON_L        = 0x06;
+  static constexpr uint8_t R_LED0_ON_H        = 0x07;
+  static constexpr uint8_t R_LED0_OFF_L       = 0x08;
+  static constexpr uint8_t R_LED0_OFF_H       = 0x09;
+
+};
+
 
 namespace pca9685_hardware_interface
 {
@@ -54,7 +135,7 @@ public:
 
 private:
   std::vector<double> hw_commands_;
-  PiPCA9685::PCA9685 pca;
+  PCA9685 pca;
   double command_to_duty_cycle(double command);
 };
 
