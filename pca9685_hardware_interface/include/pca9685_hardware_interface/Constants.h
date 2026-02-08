6acb9143c82b84d1d44b065e7b5a362d990618e9
Christian Rauch <Rauch.Christian@gmx.de>
2026-02-08 19:37:00 +0100

    remove unused constants

--- pca9685_hardware_interface/include/pca9685_hardware_interface/Constants.h
+++ pca9685_hardware_interface/include/pca9685_hardware_interface/Constants.h
@@ -7,9 +7,6 @@ namespace PiPCA9685 {
 // Registers/etc:
 constexpr uint8_t MODE1              = 0x00;
 constexpr uint8_t MODE2              = 0x01;
-constexpr uint8_t SUBADR1            = 0x02;
-constexpr uint8_t SUBADR2            = 0x03;
-constexpr uint8_t SUBADR3            = 0x04;
 constexpr uint8_t PRESCALE           = 0xFE;
 constexpr uint8_t LED0_ON_L          = 0x06;
 constexpr uint8_t LED0_ON_H          = 0x07;
@@ -24,7 +21,6 @@ constexpr uint8_t ALL_LED_OFF_H      = 0xFD;
 constexpr uint8_t RESTART            = 0x80;
 constexpr uint8_t SLEEP              = 0x10;
 constexpr uint8_t ALLCALL            = 0x01;
-constexpr uint8_t INVRT              = 0x10;
 constexpr uint8_t OUTDRV             = 0x04;
 
 }  // namespace PiPCA9685
