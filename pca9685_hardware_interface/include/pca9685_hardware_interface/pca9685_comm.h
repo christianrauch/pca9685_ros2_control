895e7df261dd088586d2adb20c908591fdeaea97
Christian Rauch <Rauch.Christian@gmx.de>
2026-02-08 11:30:54 +0100

    remove 'I2CPeripheral'

--- pca9685_hardware_interface/include/pca9685_hardware_interface/pca9685_comm.h
+++ pca9685_hardware_interface/include/pca9685_hardware_interface/pca9685_comm.h
@@ -2,11 +2,9 @@
 #define RPY_PWM_HAT_PCA9685_H
 
 #include <string>
-#include <memory>
+#include <cstdint>
 
 namespace PiPCA9685 {
-
-class I2CPeripheral;
 
 class PCA9685 {
 public:
@@ -17,13 +15,11 @@ public:
 
     void set_pwm(const int channel, const uint16_t on, const uint16_t off);
 
-    void set_all_pwm(const uint16_t on, const uint16_t off);
-
     void set_pwm_ms(const int channel, const double ms);
 
 private:
-    std::unique_ptr<I2CPeripheral> i2c_dev;
-    
+    int bus_fd;
+
     // Default frequency pulled from PCA9685 datasheet.
     double frequency = 200.0;
 
--- pca9685_hardware_interface/src/pca9685_comm.cpp
+++ pca9685_hardware_interface/src/pca9685_comm.cpp
@@ -2,25 +2,50 @@
 #include <unistd.h>
 #include <cmath>
 
-#include "pca9685_hardware_interface/Constants.h"
-#include "pca9685_hardware_interface/I2CPeripheral.h"
+#include <system_error>
+
+#include <sys/ioctl.h>
+#include <fcntl.h>
+extern "C" {
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
+}
 
 namespace PiPCA9685 {
 
+// Registers/etc:
+constexpr uint8_t R_MODE1            = 0x00;
+constexpr uint8_t R_MODE2            = 0x01;
+constexpr uint8_t R_PRESCALE         = 0xFE;
+constexpr uint8_t R_LED0_ON_L        = 0x06;
+constexpr uint8_t R_LED0_ON_H        = 0x07;
+constexpr uint8_t R_LED0_OFF_L       = 0x08;
+constexpr uint8_t R_LED0_OFF_H       = 0x09;
+
+// Bits:
+constexpr uint8_t SLEEP              = 0x10;
+
 PCA9685::PCA9685(const std::string &device, int address) {
-  i2c_dev = std::make_unique<I2CPeripheral>(device, address);
-  
-  set_all_pwm(0,0);
-  i2c_dev->WriteRegisterByte(MODE2, OUTDRV);
-  i2c_dev->WriteRegisterByte(MODE1, ALLCALL);
+  bus_fd = open(device.c_str(), O_RDWR);
+  if(bus_fd < 0) {
+    throw std::system_error(errno, std::system_category(), "Could not open i2c bus.");
+  }
+
+  if(ioctl(bus_fd, I2C_SLAVE, address) < 0) {
+    throw std::system_error(errno, std::system_category(), "Could not set peripheral address.");
+  }
+
+  i2c_smbus_write_byte_data(bus_fd, R_MODE2, 0b0000'0100); // OUTDRV
   usleep(5'000);
-  auto mode1_val = i2c_dev->ReadRegisterByte(MODE1);
+  auto mode1_val = i2c_smbus_read_byte_data(bus_fd, R_MODE1);
   mode1_val &= ~SLEEP;
-  i2c_dev->WriteRegisterByte(MODE1, mode1_val);
+  i2c_smbus_write_byte_data(bus_fd, R_MODE1, mode1_val);
   usleep(5'000);
 }
 
-PCA9685::~PCA9685() = default;
+PCA9685::~PCA9685() {
+  close(bus_fd);
+}
 
 void PCA9685::set_pwm_freq(const double freq_hz) {
   frequency = freq_hz;
@@ -32,30 +57,23 @@ void PCA9685::set_pwm_freq(const double freq_hz) {
 
   auto prescale = static_cast<int>(std::round(prescaleval));
 
-  const auto oldmode = i2c_dev->ReadRegisterByte(MODE1);
+  const auto oldmode = i2c_smbus_read_byte_data(bus_fd, R_MODE1);
 
   auto newmode = (oldmode & 0x7F) | SLEEP;
 
-  i2c_dev->WriteRegisterByte(MODE1, newmode);
-  i2c_dev->WriteRegisterByte(PRESCALE, prescale);
-  i2c_dev->WriteRegisterByte(MODE1, oldmode);
+  i2c_smbus_write_byte_data(bus_fd, R_MODE1, newmode);
+  i2c_smbus_write_byte_data(bus_fd, R_PRESCALE, prescale);
+  i2c_smbus_write_byte_data(bus_fd, R_MODE1, oldmode);
   usleep(5'000);
-  i2c_dev->WriteRegisterByte(MODE1, oldmode | RESTART);
+  i2c_smbus_write_byte_data(bus_fd, R_MODE1, oldmode | 0b1000'0000);
 }
 
 void PCA9685::set_pwm(const int channel, const uint16_t on, const uint16_t off) {
   const auto channel_offset = 4 * channel;
-  i2c_dev->WriteRegisterByte(LED0_ON_L+channel_offset, on & 0xFF);
-  i2c_dev->WriteRegisterByte(LED0_ON_H+channel_offset, on >> 8);
-  i2c_dev->WriteRegisterByte(LED0_OFF_L+channel_offset, off & 0xFF);
-  i2c_dev->WriteRegisterByte(LED0_OFF_H+channel_offset, off >> 8);
-}
-
-void PCA9685::set_all_pwm(const uint16_t on, const uint16_t off) {
-  i2c_dev->WriteRegisterByte(ALL_LED_ON_L, on & 0xFF);
-  i2c_dev->WriteRegisterByte(ALL_LED_ON_H, on >> 8);
-  i2c_dev->WriteRegisterByte(ALL_LED_OFF_L, off & 0xFF);
-  i2c_dev->WriteRegisterByte(ALL_LED_OFF_H, off >> 8);
+  i2c_smbus_write_byte_data(bus_fd, R_LED0_ON_L+channel_offset, on & 0xFF);
+  i2c_smbus_write_byte_data(bus_fd, R_LED0_ON_H+channel_offset, on >> 8);
+  i2c_smbus_write_byte_data(bus_fd, R_LED0_OFF_L+channel_offset, off & 0xFF);
+  i2c_smbus_write_byte_data(bus_fd, R_LED0_OFF_H+channel_offset, off >> 8);
 }
 
 void PCA9685::set_pwm_ms(const int channel, const double ms) {
